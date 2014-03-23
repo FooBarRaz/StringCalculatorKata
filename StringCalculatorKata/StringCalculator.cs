b8e7810fa6660c1564281f9caa635a29c5e54007
fakeplasticdroid <mehraz.hakim@gmail.com>
2014-03-23 12:43:50 -0400

    AddedInitialUnitTests

---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculatorKata
+{
+    class StringCalculator
+    {
+        internal int Add(string numbers)
+        {
+            int result;
+            if (Int32.TryParse(numbers, out result))
+            {
+                return result;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringCalculatorKata
+{
+    class StringCalculatorTest
+    {
+        private StringCalculator stringCalculator;
+
+        [SetUp]
+        public void ConstructStringCalculator()
+        {
+            stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void stringCalculatorAdd_EmptyStringReturns0() 
+        {
+            Assert.That(stringCalculator.Add(string.Empty), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AddOneNumber_ReturnsNumber()
+        {
+            Assert.That(stringCalculator.Add("5"), Is.EqualTo(5));
+        }
+
+    }
+}
